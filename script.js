8349c78d2424bdcf86fd4b4611c6ae2d826b71cc
Cristiano Vieira <cristianoviieira@gmail.com>
2022-02-16 18:56:02 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let elementoDiv = document.getElementsByTagName('section');
+const elementoDiv = document.getElementsByTagName('section');
 
-function criandoDivs(multiplo){
-    for (let index = 0; index < multiplo; index += 1) {
-        let makeDiv = document.createElement('div');
-        makeDiv.className= "quantidade"
-        elementoDiv[0].appendChild(makeDiv);
-    }
+function criandoDivs(multiplo) {
+  for (let index = 0; index < multiplo; index += 1) {
+    let makeDiv = document.createElement('div');
+    makeDiv.className = "quantidade"
+    elementoDiv[0].appendChild(makeDiv);
+  }
+  for (let index = 0; index < multiplo; index++) {
+    let conta = 0;
     for (let index = 0; index < multiplo; index++) {
-        let conta = 0;
-        for (let index = 0; index < multiplo; index++) {
-            let test = document.getElementsByClassName('quantidade')
-            let test1 = document.createElement('div')
-            test1.className= "pixel"
-            test[conta].appendChild(test1)
-            conta = conta+1;
-        }
+      let test = document.getElementsByClassName('quantidade')
+      let test1 = document.createElement('div')
+      test1.className = "pixel"
+      test[conta].appendChild(test1)
+      conta = conta + 1;
     }
+  }
 }
 
 criandoDivs(5);
@@ -23,18 +23,18 @@ criandoDivs(5);
 let clickSelecionar = document.querySelectorAll('#color-palette div')
 let clickSelecionarPixel = document.querySelectorAll('.pixel')
 
-function apagando(test){
-    for (let index = 0; index < 4; index++) {
-        test[index].className ='color'
-        
-    }
+function apagando(test) {
+  for (let index = 0; index < 4; index++) {
+    test[index].className = 'color'
+
+  }
 }
 
 
-function recebeclickPaleta(eventoDeOrigen){
-    apagando(clickSelecionar);
-    let selecionando = eventoDeOrigen.target;
-    selecionando.className='color selected';
+function recebeclickPaleta(eventoDeOrigen) {
+  apagando(clickSelecionar);
+  let selecionando = eventoDeOrigen.target;
+  selecionando.className = 'color selected';
 }
 
 clickSelecionar[0].addEventListener("click", recebeclickPaleta);
@@ -48,13 +48,13 @@ let tentando;
 
 const board = document.querySelector("#pixel-board")
 
-function recebiClickPixel(event){
-    let selecionandoPixel = event.target;
-    if (selecionandoPixel.classList.contains('pixel')) {
-        let selecionado = document.querySelector('.selected')
-        let corSelecionada = window.getComputedStyle(selecionado)
-        let cor = corSelecionada.getPropertyValue('background-color')
-        selecionandoPixel.style.backgroundColor = cor;
-    }
+function recebiClickPixel(event) {
+  let selecionandoPixel = event.target;
+  if (selecionandoPixel.classList.contains('pixel')) {
+    let selecionado = document.querySelector('.selected')
+    let corSelecionada = window.getComputedStyle(selecionado)
+    let cor = corSelecionada.getPropertyValue('background-color')
+    selecionandoPixel.style.backgroundColor = cor;
+  }
 }
 board.addEventListener("click", recebiClickPixel);
