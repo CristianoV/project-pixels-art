8ec8dd5ccff707da248aba354d9464280593fdd5
Cristiano Vieira <cristianoviieira@gmail.com>
2022-02-16 15:49:14 -0300

    finalizando o setimo requisito

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 let elementoDiv = document.getElementsByTagName('section');
 
-
-function criandoDivs(primeiro, segundo){
-    for (let index = 0; index < primeiro; index += 1) {
+function criandoDivs(multiplo){
+    for (let index = 0; index < multiplo; index += 1) {
         let makeDiv = document.createElement('div');
         makeDiv.className= "quantidade"
         elementoDiv[0].appendChild(makeDiv);
-
     }
-    for (let index = 0; index < segundo; index++) {
+    for (let index = 0; index < multiplo; index++) {
         let conta = 0;
-        for (let index = 0; index < segundo; index++) {
+        for (let index = 0; index < multiplo; index++) {
             let test = document.getElementsByClassName('quantidade')
             let test1 = document.createElement('div')
             test1.className= "pixel"
@@ -18,7 +16,28 @@ function criandoDivs(primeiro, segundo){
             conta = conta+1;
         }
     }
-
 }
 
-criandoDivs(5, 5);
+criandoDivs(5);
+
+let clickSelecionar = document.querySelectorAll('#color-palette div')
+
+
+function apagando(test){
+    for (let index = 0; index < 4; index++) {
+        test[index].className ='color'
+        
+    }
+}
+
+
+function recebiClick(eventoDeOrigen){
+    apagando(clickSelecionar);
+    let selecionando = eventoDeOrigen.target;
+    selecionando.className='color selected';
+}
+
+clickSelecionar[0].addEventListener("click", recebiClick);
+clickSelecionar[1].addEventListener("click", recebiClick);
+clickSelecionar[2].addEventListener("click", recebiClick);
+clickSelecionar[3].addEventListener("click", recebiClick);
